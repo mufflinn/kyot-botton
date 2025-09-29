3241c79a722599c1db5494c3aca5f4bb7d40f6a5
mufflinn <fhonly1@gmail.com>
2025-09-29 09:23:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-gsap.to(".title", {});
+<!-- GSAP Core -->
+<script src="https://unpkg.com/gsap@3/dist/gsap.min.js"></script>
+<!-- ScrollTrigger Plugin -->
+<script src="https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js"></script>
+
+<script>
+  // Register the plugin
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Example animation for your .title
+  gsap.to(".title", {
+    x: 150, 
+    rotation: 360,
+    duration: 2,
+    scrollTrigger: {
+      trigger: ".title",
+      start: "top 80%",
+      end: "top 20%",
+      scrub: true,
+      markers: true // <- shows start/end points (remove later)
+    }
+  });
+</script>
